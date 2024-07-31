95838b54c89849947f8ca4612757e84c08757ac4
Jobey Farias <115730827+Jobeyy@users.noreply.github.com>
2024-07-31 16:22:23 -0500

    adding auth to program

---
+++ pantry-tracker/src/app/context/AuthContext.js
@@ -0,0 +1,21 @@
+'use client'
+import { useContext, createContext, useState, useEffect } from "react";
+
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+
+
+  return (
+    <AuthContext.Provider value={{user}}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const UserAuth = () => {
+  return useContext(AuthContext);
+};
